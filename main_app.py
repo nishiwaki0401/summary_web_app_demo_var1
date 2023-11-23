c41b8b532cbb8ef7993b2fa1db8eb9866bebc625
nishiwaki0401 <150115004+nishiwaki0401@users.noreply.github.com>
2023-11-24 00:57:55 +0900

    Create main_app.py

---
+++ main_app.py
@@ -0,0 +1,72 @@
+import streamlit as st
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import (
+    SystemMessage,
+    HumanMessage,
+    AIMessage
+)
+from langchain.callbacks import get_openai_callback
+
+def init_page():
+    st.set_page_config(
+        page_title="要約アプリ",
+        page_icon="🧠"
+    )
+    st.header("要約アプリ 🧠")
+    st.sidebar.title("モデル選択")
+
+def init_messages():
+    clear_button = st.sidebar.button("履歴削除", key="clear")
+    if clear_button or "messages" not in st.session_state:
+        st.session_state.messages = [
+            SystemMessage(content="デモ段階であるため、ただchatgptのapiを使用してwebappを作成しただけになっているが今後要約アプリとして工夫していく")
+        ]
+        st.session_state.costs = []
+
+def select_model():
+    model = st.sidebar.radio("Choose a model:", ("GPT-3.5", "GPT-4"))
+    if model == "GPT-3.5":
+        model_name = "gpt-3.5-turbo"
+    else:
+        model_name = "gpt-4"
+
+    temperature = st.sidebar.slider("Temperature:", min_value=0.0, max_value=2.0, value=0.0, step=0.01)
+
+    return ChatOpenAI(temperature=temperature, model_name=model_name)
+
+def get_answer(llm, messages):
+    with get_openai_callback() as cb:
+        answer = llm(messages)
+    return answer.content, cb.total_cost
+
+def main():
+    init_page()
+    llm = select_model()
+    init_messages()
+
+    user_input = st.text_area("質問したい内容を打って下さい")
+
+    if st.button("要約する"):
+        st.session_state.messages.append(HumanMessage(content=user_input))
+        with st.spinner("ChatGPT is typing ..."):
+            answer, cost = get_answer(llm, st.session_state.messages)
+        st.session_state.messages.append(AIMessage(content=answer))
+        st.session_state.costs.append(cost)
+
+    messages = st.session_state.get('messages', [])
+    for message in messages:
+        if isinstance(message, AIMessage):
+            st.markdown(message.content)
+        elif isinstance(message, HumanMessage):
+            st.markdown(message.content)
+        else:  # isinstance(message, SystemMessage):
+            st.write(f"System message: {message.content}")
+
+    costs = st.session_state.get('costs', [])
+    st.sidebar.markdown("## Costs")
+    st.sidebar.markdown(f"**Total cost: ${sum(costs):.5f}**")
+    for cost in costs:
+        st.sidebar.markdown(f"- ${cost:.5f}")
+
+if __name__ == '__main__':
+    main()
