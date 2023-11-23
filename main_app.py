29ada9c21dc30aae60a30503b66668ca3967939e
nishiwaki0401 <150115004+nishiwaki0401@users.noreply.github.com>
2023-11-24 01:38:06 +0900

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -1,25 +1,21 @@
 import streamlit as st
 from langchain.chat_models import ChatOpenAI
-from langchain.schema import SystemMessage, HumanMessage, AIMessage
 from langchain.callbacks import get_openai_callback
+
 from langchain.prompts import PromptTemplate
 from langchain.chains.summarize import load_summarize_chain
+from langchain.document_loaders import YoutubeLoader
+
 
 def init_page():
     st.set_page_config(
-        page_title="要約アプリ",
-        page_icon="🧠"
+        page_title="Youtube Summarizer",
+        page_icon="🤗"
     )
-    st.header("要約アプリ 🧠")
-    st.sidebar.title("モデル選択")
+    st.header("Youtube Summarizer 🤗")
+    st.sidebar.title("Options")
+    st.session_state.costs = []
 
-def init_messages():
-    clear_button = st.sidebar.button("履歴削除", key="clear")
-    if clear_button or "messages" not in st.session_state:
-        st.session_state.messages = [
-            SystemMessage(content="デモ段階であるため、ただchatgptのapiを使用してwebappを作成しただけになっているが今後要約アプリとして工夫していく")
-        ]
-        st.session_state.costs = []
 
 def select_model():
     model = st.sidebar.radio("Choose a model:", ("GPT-3.5", "GPT-4"))
@@ -28,15 +24,29 @@ def select_model():
     else:
         model_name = "gpt-4"
 
-    temperature = st.sidebar.slider("Temperature:", min_value=0.0, max_value=2.0, value=0.0, step=0.01)
+    return ChatOpenAI(temperature=0, model_name=model_name)
 
-    return ChatOpenAI(temperature=temperature, model_name=model_name)
 
-def summarize(llm, text):
-    prompt_template = """以下のテキストの簡潔な日本語要約を書いてください。
+def get_url_input():
+    url = st.text_input("Youtube URL: ", key="input")
+    return url
+
+
+def get_document(url):
+    with st.spinner("Fetching Content ..."):
+        loader = YoutubeLoader.from_youtube_url(
+            url,
+            add_video_info=True,  # タイトルや再生数も取得できる
+            language=['en', 'ja']  # 英語→日本語の優先順位で字幕を取得
+        )
+        return loader.load()
+
+
+def summarize(llm, docs):
+    prompt_template = """Write a concise Japanese summary of the following transcript of Youtube Video.
 
 ============
-
+    
 {text}
 
 ============
@@ -47,42 +57,45 @@ def summarize(llm, text):
     PROMPT = PromptTemplate(template=prompt_template, input_variables=["text"])
 
     with get_openai_callback() as cb:
-        chain = load_summarize_chain(
+        chain = load_summarize_chain( 
             llm,
             chain_type="stuff",
             verbose=True,
             prompt=PROMPT
         )
-        response = chain({"input_documents": [text]}, return_only_outputs=True)
-
+        response = chain({"input_documents": docs}, return_only_outputs=True)
+        
     return response['output_text'], cb.total_cost
+
 
 def main():
     init_page()
     llm = select_model()
-    init_messages()
 
-    user_input = st.text_area("要約したいテキストを入力してください")
+    container = st.container()
+    response_container = st.container()
 
-    if st.button("要約する"):
-        st.session_state.messages.append(HumanMessage(content=user_input))
-        with st.spinner("ChatGPT が要約しています..."):
-            summary, cost = summarize(llm, user_input)
-        st.session_state.messages.append(AIMessage(content=summary))
-        st.session_state.costs.append(cost)
-
-    messages = st.session_state.get('messages', [])
-    for message in messages:
-        if isinstance(message, AIMessage):
-            st.markdown(message.content)
-        elif isinstance(message, HumanMessage):
-            st.markdown(message.content)
+    with container:
+        url = get_url_input()
+        if url:
+            document = get_document(url)
+            with st.spinner("ChatGPT is typing ..."):
+                output_text, cost = summarize(llm, document)
+            st.session_state.costs.append(cost)
         else:
-            st.write(f"System message: {message.content}")
+            output_text = None
+
+    if output_text:
+        with response_container:
+            st.markdown("## Summary")
+            st.write(output_text)
+            st.markdown("---")
+            st.markdown("## Original Text")
+            st.write(document)
 
     costs = st.session_state.get('costs', [])
-    st.sidebar.markdown("## コスト")
-    st.sidebar.markdown(f"**総コスト: ${sum(costs):.5f}**")
+    st.sidebar.markdown("## Costs")
+    st.sidebar.markdown(f"**Total cost: ${sum(costs):.5f}**")
     for cost in costs:
         st.sidebar.markdown(f"- ${cost:.5f}")
 
