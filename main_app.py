a191d3b08547078b1419db397ced910e2f98e23b
nishiwaki0401 <150115004+nishiwaki0401@users.noreply.github.com>
2023-11-24 02:10:32 +0900

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -1,10 +1,8 @@
 import streamlit as st
 from langchain.chat_models import ChatOpenAI
 from langchain.callbacks import get_openai_callback
-
 from langchain.prompts import PromptTemplate
 from langchain.chains.summarize import load_summarize_chain
-from langchain.document_loaders import YoutubeLoader
 
 
 def init_page():
@@ -27,19 +25,9 @@ def select_model():
     return ChatOpenAI(temperature=0, model_name=model_name)
 
 
-def get_url_input():
-    url = st.text_input("Youtube URL: ", key="input")
-    return url
-
-
-def get_document(url):
-    with st.spinner("Fetching Content ..."):
-        loader = YoutubeLoader.from_youtube_url(
-            url,
-            add_video_info=True,  # タイトルや再生数も取得できる
-            language=['en', 'ja']  # 英語→日本語の優先順位で字幕を取得
-        )
-        return loader.load()
+def get_text_input():
+    text_input = st.text_area("テキストを入力してください:", key="input", height=200)
+    return text_input
 
 
 def summarize(llm, docs):
@@ -76,14 +64,16 @@ def main():
     response_container = st.container()
 
     with container:
-        url = get_url_input()
-        if url:
-            document = get_document(url)
-            with st.spinner("ChatGPT is typing ..."):
-                output_text, cost = summarize(llm, document)
-            st.session_state.costs.append(cost)
-        else:
-            output_text = None
+        text_input = get_text_input()
+
+    if text_input:
+        document = [{"content": text_input, "title": "User Input"}]
+
+        with st.spinner("ChatGPT is typing ..."):
+            output_text, cost = summarize(llm, document)
+        st.session_state.costs.append(cost)
+    else:
+        output_text = None
 
     if output_text:
         with response_container:
@@ -91,8 +81,8 @@ def main():
             st.write(output_text)
             st.markdown("---")
             st.markdown("## Original Text")
-            st.write(document)
-
+            st.write("User Input")
+    
     costs = st.session_state.get('costs', [])
     st.sidebar.markdown("## Costs")
     st.sidebar.markdown(f"**Total cost: ${sum(costs):.5f}**")
